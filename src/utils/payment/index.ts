5dcccddb272082cba9a027bc857695530cfccd01
walkwel-tech <developer.walkwel@gmail.com>
2023-07-05 14:56:14 +0530

    Added Logs

--- src/utils/payment/index.ts
+++ src/utils/payment/index.ts
@@ -4,11 +4,15 @@ import { addCreditsToBuyer } from "./addBuyerCredit";
 import { attemptToPayment, attemptToPaymentBy_PaymentMethods, createSession, customerPaymentMethods, refundPayment } from "./createPaymentToRYFT";
 
 export const managePayments = (params: PaymentInput) => {
+  console.log('DEBUGGER params--->>>>>',params)
     return new Promise((resolve, reject) => {
       createSession(params)
         .then((response: any) => {
+          console.log("DEBUGGER 1--->", response.data)
           attemptToPayment(response,params)
-            .then((data) => {
+            .then((data:any) => {
+              console.log("DEBUGGER 2--->", data.data)
+
               addCreditsToBuyer(params).then((res)=>{
                   resolve(res)
               }).catch((err)=>{
@@ -50,14 +54,18 @@ export const fetchPaymentSessionById = (id:string)=>{
 
 
   export const  managePaymentsByPaymentMethods = (params:any) => {
+    console.log('DEBUGGER params by payment method--->>>>>',params)
+
     return new Promise((resolve, reject) => {
       createSession(params)
         .then((res: any) => {
-        
-          
+          console.log("DEBUGGER 1.1--->", res.data)          
           customerPaymentMethods(res).then((response:any)=>{
+            console.log("DEBUGGER 1.2--->", response.data)
+
               attemptToPaymentBy_PaymentMethods(response,res.data.clientSecret)
-            .then((data) => {
+            .then((data:any) => {
+              console.log("DEBUGGER 1.3--->", data.data)
               addCreditsToBuyer(params).then((res)=>{
                 console.log("credits added")
 
