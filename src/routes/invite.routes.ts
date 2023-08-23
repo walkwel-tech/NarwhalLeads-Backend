2364520233fe2218e19f69f4b8235ef1b8c5b1d7
walkwel-tech <developer.walkwel@gmail.com>
2023-08-23 11:17:34 +0530

    1.0.13 - API Integration for Business Details Submission - New Signup Notification Management on Admin Dashboard

--- src/routes/invite.routes.ts
+++ src/routes/invite.routes.ts
@@ -4,11 +4,19 @@ import { invitedUsersController } from '../app/Controllers';
 import { Auth } from '../app/Middlewares';
 
 const invites: Router = Router();
+invites.post('/subscriber',Auth, invitedUsersController.addSubscribers);
+invites.delete('/subscriber/:id',Auth, invitedUsersController.deleteSubscriber);
 invites.post('/',Auth, invitedUsersController.create);
 invites.get('/',Auth, invitedUsersController.show);
 invites.delete('/:id',Auth, invitedUsersController.delete);
 invites.patch('/:id',Auth, invitedUsersController.update);
 invites.post('/:id',Auth, invitedUsersController.update);
+invites.get('/subscriber',Auth, invitedUsersController.indexSubscriber);
+
+
+
+
+
 
 
 export default invites;
