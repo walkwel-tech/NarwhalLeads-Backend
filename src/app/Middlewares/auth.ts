683a27cc59d42b142e0c03aedc8055662c35c72e
walkwel-tech <developer.walkwel@gmail.com>
2023-09-28 17:22:33 +0530

    1.0.67 - Modifications in non-billable module

--- src/app/Middlewares/auth.ts
+++ src/app/Middlewares/auth.ts
@@ -9,14 +9,14 @@ export default function Auth(req: Request, res: Response, next: NextFunction) {
   return passport.authenticate(
     "jwt",
     { session: false },
-    async (err:any, payload: UserInterface) => {
+    async (err: any, payload: UserInterface) => {
       if (err) {
         return res
           .status(500)
           .json({ error: { message: "Something went wrong" } });
       }
-      const tokenUser:any=await User.findById(payload.id)
-      if (!payload || (!tokenUser)) {
+      const tokenUser: any = await User.findById(payload.id);
+      if (!payload || !tokenUser) {
         return res
           .status(401)
           .json({ error: { message: "Invalid Token. Access Denied!" } });
@@ -24,12 +24,10 @@ export default function Auth(req: Request, res: Response, next: NextFunction) {
       if (!tokenUser?.isActive) {
         return res.status(401).json({ error: { message: "User not Active!" } });
       }
-      if (tokenUser?.isDeleted ) {
-        return res
-          .status(401)
-          .json({
-            error: { message: "User is deleted.Please contact admin!" },
-          });
+      if (tokenUser?.isDeleted) {
+        return res.status(401).json({
+          error: { message: "User is deleted.Please contact admin!" },
+        });
       }
       req.user = tokenUser || undefined;
       return next();
--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,26 +1,24 @@
+import { Router } from "express";
 
-
-import { Router } from 'express';
-
-import { AuthController } from '../app/Controllers';
-import { Auth, OnlyAdmins } from '../app/Middlewares';
+import { AuthController } from "../app/Controllers";
+import { Auth, OnlyAdmins } from "../app/Middlewares";
 
 const auth: Router = Router();
-auth.get('/map',AuthController.showMapFile)
-auth.get('/map/ireland',AuthController.showMapFileForIreland)
+auth.get("/map", AuthController.showMapFile);
+auth.get("/map/ireland", AuthController.showMapFileForIreland);
 // auth.get('/mapForLabel',AuthController.showMapFileForLabel)
-auth.get('/',Auth,AuthController.auth)
-auth.get('/me',Auth,AuthController.me)
-auth.patch('/test',AuthController.test)
-auth.post('/register', AuthController.register);
-auth.post('/return-url', AuthController.returnUrlApi);
-auth.post('/activeUser/:id',OnlyAdmins, AuthController.activeUser);
-auth.post('/admin/register', AuthController.adminRegister);
-auth.post('/login', AuthController.login);
-auth.post('/adminLogin', AuthController.adminLogin);
-auth.post('/checkUser', AuthController.checkUser);
-auth.post('/forgetPassword', AuthController.forgetPassword);
-auth.get('/user-status/:id',OnlyAdmins,AuthController.userStatus)
-
+auth.get("/", Auth, AuthController.auth);
+auth.get("/me", Auth, AuthController.me);
+auth.patch("/test", AuthController.test);
+auth.post("/register", AuthController.register);
+auth.post("/return-url", AuthController.returnUrlApi);
+auth.post("/activeUser/:id", OnlyAdmins, AuthController.activeUser);
+auth.post("/admin/register", AuthController.adminRegister);
+auth.post("/login", AuthController.login);
+auth.post("/adminLogin", AuthController.adminLogin);
+auth.post("/checkUser", AuthController.checkUser);
+auth.post("/forgetPassword", AuthController.forgetPassword);
+auth.get("/user-status/:id", OnlyAdmins, AuthController.userStatus);
+auth.post("/createOnRyft", Auth, AuthController.createCustomerOnRyft);
 
 export default auth;
