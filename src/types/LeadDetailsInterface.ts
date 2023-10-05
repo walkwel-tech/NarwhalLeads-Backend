a87f010ce00a928d33d30ce19c236609d4362a69
walkwel-tech <developer.walkwel@gmail.com>
2023-10-05 13:30:58 +0530

    1.0.77 - Added function to check object id in LeadDetailsInterface.ts

--- src/types/LeadDetailsInterface.ts
+++ src/types/LeadDetailsInterface.ts
@@ -2,18 +2,27 @@ import { Document, Types } from "mongoose";
 
 export interface UserLeadsDetailsInterface extends Document {
   _id: Types.ObjectId;
-  userId:Types.ObjectId;
+  userId: Types.ObjectId;
   total: number;
   daily: number;
   weekly: number;
   monthly: number;
   leadSchedule: [];
-  postCodeTargettingList:[];
-  leadAlertsFrequency:string;
-  zapierUrl:string;
-  sendDataToZapier:boolean;
+  postCodeTargettingList: [];
+  leadAlertsFrequency: string;
+  zapierUrl: string;
+  sendDataToZapier: boolean;
   createdAt: Date;
-  isDeleted:  boolean,
-  deletedAt:  Date,
-  dailyLeadCost: number
+  isDeleted: boolean;
+  deletedAt: Date;
+  dailyLeadCost: number;
+}
+
+export function isUserLeadDetailsObject(
+  b: unknown
+): b is UserLeadsDetailsInterface {
+  return (
+    typeof b !== "string" &&
+    Object.keys(b as Object).includes("leadAlertsFrequency")
+  );
 }
