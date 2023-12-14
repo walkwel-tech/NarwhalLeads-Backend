d6f8e80705c038fe8968851b84337ad14de70b3f
walkwel-tech <developer.walkwel@gmail.com>
2023-12-14 12:59:41 +0530

    1.2.43 - Bug fixes related to Account stats manager dashboard and partners page.

--- src/routes/ads.routes.ts
+++ src/routes/ads.routes.ts
@@ -1,5 +1,4 @@
 import { Router } from "express";
-import { OnlyAdmins } from "../app/Middlewares";
 import { AdsController } from "../app/Controllers/ads.controller";
 import multer from "multer";
 import { FileEnum } from "../types/FileEnum";
