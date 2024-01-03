fe608adfd4589ce138042ef47f04b66884396ab2
walkwel-tech <developer.walkwel@gmail.com>
2024-01-03 14:55:41 +0530

    1.2.64 - Included VAT in currencyConstants.ts for "USA"

--- src/utils/constantFiles/currencyConstants.ts
+++ src/utils/constantFiles/currencyConstants.ts
@@ -1,5 +1,5 @@
 export const countryCurrency = [
-  { value: "USD", label: "$(USD)", country: "USA" },
+  { value: "USD", label: "$(USD)", country: "USA", VAT: 20 },
   { value: "EUR", label: "€(EUR)", country: "Ireland" },
   { value: "GBP", label: "£(GBP)", country: "UK", VAT: 20 },
 ];
