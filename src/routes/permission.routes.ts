f9672ca5d0bca5ee80e0cc10c6f4439b2f2585f3
akash sharma <akash.sharma@walkweltech.com>
2024-02-28 14:50:12 +0530

    permissions check added

--- src/routes/permission.routes.ts
+++ src/routes/permission.routes.ts
@@ -1,5 +1,7 @@
 import { Router } from "express";
 
+import { checkPermissions } from "../app/Middlewares/roleBasedAuthentication";
+import { MODULE, PERMISSIONS } from "../utils/Enums/permissions.enum";
 import { PermissionController } from "../app/Controllers/permission.controller";
 import { OnlyAdmins } from "../app/Middlewares";
 
@@ -7,8 +9,18 @@ const permissionRoutes: Router = Router();
 
 permissionRoutes.get("/", OnlyAdmins, PermissionController.view);
 permissionRoutes.post("/", OnlyAdmins, PermissionController.create);
-permissionRoutes.get("/user/:id", PermissionController.getUserPermissions);
-permissionRoutes.post("/user/:id", OnlyAdmins, PermissionController.updateUserPermissions);
+permissionRoutes.get("/user/:id",
+    OnlyAdmins,
+    checkPermissions([
+        { module: MODULE.ROLE_PERMISSIONS, permission: PERMISSIONS.READ },
+    ]),
+    PermissionController.getUserPermissions);
+permissionRoutes.post("/user/:id", 
+    OnlyAdmins, 
+    checkPermissions([
+        { module: MODULE.ROLE_PERMISSIONS, permission: PERMISSIONS.UPDATE },
+    ]),
+    PermissionController.updateUserPermissions);
 permissionRoutes.delete("/:id", OnlyAdmins, PermissionController.delete);
 permissionRoutes.post("/:id", OnlyAdmins, PermissionController.update);
 
