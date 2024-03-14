9188e3ad7348b10ef22dab6d57120ef415a6f441
Lakshay Verma <lakshay.verma@walkweltech.com>
2024-03-14 15:22:26 +0530

    Stop CRON for buyer sync

--- src/app/AutoUpdateTasks/index.ts
+++ src/app/AutoUpdateTasks/index.ts
@@ -1,20 +1,19 @@
-import {cronBuyerStatus} from "./buyerActivations";
-import { updateReportStatus } from "./ReportingStatusUpdate/updateReportStatus";
-import { activityLogs } from "./activityLogs";
+import {activityLogs} from "./activityLogs";
 import {autoChargePayment, weeklyPayment} from "./autoCharge";
-import { db_dump } from "./db.dump";
-import { autoWebhookURLHitLeadSubmission } from "./leadStatusUpdateWebhook";
-import { mailForTotalLeadsInDay } from "./sendMail";
-import { userSignupReminder } from "./userSignupReminder";
+import {db_dump} from "./db.dump";
+import {autoWebhookURLHitLeadSubmission} from "./leadStatusUpdateWebhook";
+import {updateReportStatus} from "./ReportingStatusUpdate/updateReportStatus";
+import {mailForTotalLeadsInDay} from "./sendMail";
+import {userSignupReminder} from "./userSignupReminder";
 
 export const autoUpdateTasks = () => {
-  autoChargePayment();
-  updateReportStatus()
-  weeklyPayment();
-  mailForTotalLeadsInDay();
-  autoWebhookURLHitLeadSubmission();
-  activityLogs();
-  db_dump();
-  userSignupReminder();
-  cronBuyerStatus();
+    autoChargePayment();
+    updateReportStatus()
+    weeklyPayment();
+    mailForTotalLeadsInDay();
+    autoWebhookURLHitLeadSubmission();
+    activityLogs();
+    db_dump();
+    userSignupReminder();
+    // cronBuyerStatus();
 };
