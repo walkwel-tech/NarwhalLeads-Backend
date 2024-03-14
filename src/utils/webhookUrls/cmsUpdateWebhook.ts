46cc8f37a39a9a0c31ce80286f2b22f2a4d3b1ef
Lakshay Verma <lakshay.verma@walkweltech.com>
2024-03-14 13:04:07 +0530

    Allow CMS hooks to be debugged better

--- src/utils/webhookUrls/cmsUpdateWebhook.ts
+++ src/utils/webhookUrls/cmsUpdateWebhook.ts
@@ -1,38 +1,40 @@
 import axios from "axios";
+
+import {GET, PATCH, POST, PUT} from "../constantFiles/HttpMethods";
 import logger from "../winstonLogger/logger";
 
-import { GET, PATCH, POST, PUT } from "../constantFiles/HttpMethods";
 interface ICmsUpdateBody {
-  [key: string]: any;
+    [key: string]: any;
 }
 
 
 type HttpMethod = typeof POST | typeof PUT | typeof PATCH | typeof GET;
 
 export const cmsUpdateWebhook = async (
-  end_point: string,
-  Method: HttpMethod,
-  body: ICmsUpdateBody
+    end_point: string,
+    Method: HttpMethod,
+    body: ICmsUpdateBody
 ) => {
-  try {
-    
+    try {
+        let hookURL = (process.env.CMS_WEBHOOK_DEBUG_URL)
+            ? process.env.CMS_WEBHOOK_DEBUG_URL
+            : `${process.env.CMS_WEBHOOK_BASE_URL}${end_point}`;
+        let config = {
+            method: Method,
+            url: hookURL,
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${process.env.CMS_UPDATE_BUYER_WEBHOOK_KEY}`,
+                "X-Intended-Route": end_point
+            },
+            data: body
+        };
 
-
-    let config = {
-      method: Method,
-      url: `${process.env.CMS_WEBHOOK_BASE_URL}${end_point}`,
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${process.env.CMS_UPDATE_BUYER_WEBHOOK_KEY}`,
-      },
-      data :body
-    };
-
-    const res = await axios(config);
-    logger.info("CMS updated successfully", res.data);
-    return res.data;
-  } catch (err) {
-    logger.error("Error in updating cms ", err.response);
-    return err.response;
-  }
+        const res = await axios(config);
+        logger.info("CMS updated successfully", res.data);
+        return res.data;
+    } catch (err) {
+        logger.error("Error in updating cms ", err.response);
+        return err.response;
+    }
 };
